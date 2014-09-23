8ace9d37ef909968363bff35ff8f6535da54abd6
AlstonLin <alston_lin812@hotmail.com>
2014-09-23 00:08:40 -0400

    0.9.0 - Initial commit for this 0.9.x

--- app/build/generated/source/r/debug/com/trutech/calculall/R.java
+++ app/build/generated/source/r/debug/com/trutech/calculall/R.java
@@ -16,6 +16,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int mybutton=0x7f020001;
     }
     public static final class id {
         public static final int AddButton=0x7f07005d;
