f841b0efecd968c51ebf22b94232624d0fc68d26
Jason Fok <jason.fok@hotmail.com>
2014-11-10 02:36:27 -0500

    0.10.7 - Made buttons bigger

--- app/src/main/java/com/trutech/calculall/VectorFactory.java
+++ app/src/main/java/com/trutech/calculall/VectorFactory.java
@@ -7,28 +7,28 @@ import java.util.ArrayList;
  */
 public class VectorFactory {
     public static Vector makeU(){
-        return new Vector(Vector.U, "U"){
+        return new Vector(Vector.U, "u"){
             public ArrayList<Token> getVector(){
                 return Vector.u_value;
             }
         };
     }
     public static Vector makeV(){
-        return new Vector(Vector.V, "V"){
+        return new Vector(Vector.V, "v"){
             public ArrayList<Token> getVector(){
                 return Vector.v_value;
             }
         };
     }
     public static Vector makeW(){
-        return new Vector(Vector.W, "W"){
+        return new Vector(Vector.W, "w"){
             public ArrayList<Token> getVector(){
                 return Vector.w_value;
             }
         };
     }
     public static Vector makeS(){
-        return new Vector(Vector.S, "S"){
+        return new Vector(Vector.S, "s"){
             public ArrayList<Token> getVector(){
                 return Vector.s_value;
             }
