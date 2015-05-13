683da7590a34861eee1b412ff476cfe12c7d15b0
Jason Fok <jason.fok@hotmail.com>
2015-05-12 23:15:13 -0400

    Attempted to create constants using enum and attempted to display a popup

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -170,12 +170,12 @@ public final class R {
 		public static final int common_open_on_phone = 0x7f080039;
 		public static final int common_signin_button_text = 0x7f08003a;
 		public static final int common_signin_button_text_long = 0x7f08003b;
-		public static final int create_calendar_message = 0x7f08003d;
-		public static final int create_calendar_title = 0x7f08003e;
-		public static final int decline = 0x7f080044;
-		public static final int store_picture_message = 0x7f08009b;
-		public static final int store_picture_title = 0x7f08009c;
-		public static final int wallet_buy_button_place_holder = 0x7f0800ba;
+		public static final int create_calendar_message = 0x7f08003e;
+		public static final int create_calendar_title = 0x7f08003f;
+		public static final int decline = 0x7f080045;
+		public static final int store_picture_message = 0x7f08009c;
+		public static final int store_picture_title = 0x7f08009d;
+		public static final int wallet_buy_button_place_holder = 0x7f0800bb;
 	}
 	public static final class style {
 		public static final int Theme_IAPTheme = 0x7f090001;
