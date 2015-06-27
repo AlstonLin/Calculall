e7857b521533217de9b582f8058591a95ca06588
davidlky <david-lky@hotmail.com>
2015-06-27 12:21:09 -0400

    Themes Menu Updated!

--- app/src/main/java/com/trutechinnovations/calculall/SettingsActivity.java
+++ app/src/main/java/com/trutechinnovations/calculall/SettingsActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
